076b3e98927555ba383c8a27e8d5310710ea4f37
LPHusarz <lphusarz@gmail.com>
2019-04-01 23:00:38 +0200

    uploading new Server. Source from: http://androidsrc.net/

--- RPSPTMProject/server/ServerRockPaperScisoris/app/src/main/java/pl/a517435708/serverrockpaperscisoris/MainActivity.java
+++ RPSPTMProject/server/ServerRockPaperScisoris/app/src/main/java/pl/a517435708/serverrockpaperscisoris/MainActivity.java
@@ -1,63 +1,29 @@
 package pl.a517435708.serverrockpaperscisoris;
-
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
 import android.widget.TextView;
 
-import pl.a517435708.serverrockpaperscisoris.server.ServerRPSSocket;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
 
-    ServerRPSSocket server = new ServerRPSSocket(null);
+    Server server;
+    TextView infoip, msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        TextView textView = findViewById(R.id.label2);
-
-        textView.setText(server.listen());
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAddress() + ":" + server.getPort());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
     }
 }
+
+//Source code skopiowany z http://androidsrc.net/android-client-server-using-sockets-server-implementation/
